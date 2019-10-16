77c6494ddff0756e91012fa18f53f66a2367b433
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-16 16:25:35 -0500

    Add Getters and setters

--- src/com/excercise10oop/model/Cat.java
+++ src/com/excercise10oop/model/Cat.java
@@ -1,5 +1,56 @@
 package com.excercise10oop.model;
 
-public class Cat {
-
+public class Cat 
+{
+	//Properties
+	private int id;
+	private String name;
+	private int age;
+	
+	//Constructors
+	public Cat() 
+	{
+	}
+	public Cat(int id, String name, int age)
+	{
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	//Getters and setters
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	//ToString method
+	public String ToString() 
+	{
+		return "Id:["+id+"],name:["+name+"], age:["+age+"]";
+	}
+	
 }
