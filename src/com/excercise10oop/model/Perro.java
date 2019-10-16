4dfab38c15afd03ed7db60b66742fbb68bd7e6de
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-16 13:25:56 -0500

    Create new class Perro

---
+++ src/com/excercise10oop/model/Perro.java
@@ -0,0 +1,16 @@
+package com.excercise10oop.model;
+
+public class Perro 
+{
+	public int id;
+	private String name;
+	protected String breed;
+	public int age;
+	public double Height;
+	
+	public Perro(int id, String name, String breed, int age, double height)
+	{
+		
+	}
+	public Perro() {}
+}
